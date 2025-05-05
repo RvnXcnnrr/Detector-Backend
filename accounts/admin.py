26f2c003e091dd55fc194d4ffdb7b57fcb5617c9
Javy Rodillon <reyfoxconner@gmail.com>
2025-05-05 20:45:05 +0800

    Remove unused accounts app and related files, including migrations and database

--- accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import MotionEvent
-
-@admin.register(MotionEvent)
-class MotionEventAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'timestamp', 'location')
-    list_filter = ('user', 'timestamp')
-    search_fields = ('location',)
